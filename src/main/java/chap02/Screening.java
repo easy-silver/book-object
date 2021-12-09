c309cae403a26cd0aa0d3fe55fb7240c1b78f382
Timo <developer.timo@gmail.com>
2021-12-10 02:44:43 +0900

    [CHAPTER 02] 객체지향 프로그래밍 * 영화 예매 시스템 * 협력을 위한 영화 예매 시스템 도메인 생성 * p.37 ~ p.54

---
+++ src/main/java/chap02/Screening.java
@@ -0,0 +1,41 @@
+package chap02;
+
+import java.time.LocalDateTime;
+
+/**
+ * 상영
+ */
+public class Screening {
+
+    private Movie movie;
+    private int sequence;
+    private LocalDateTime whenScreened;
+
+    public Screening(Movie movie, int sequence, LocalDateTime whenScreened) {
+        this.movie = movie;
+        this.sequence = sequence;
+        this.whenScreened = whenScreened;
+    }
+
+    public LocalDateTime getStartTime() {
+        return whenScreened;
+    }
+
+    public boolean isSequence(int sequence) {
+        return this.sequence == sequence;
+    }
+
+    public Money getMovieFee() {
+        return movie.getFee();
+    }
+
+    public Reservation reserve(Customer customer, int audienceCount) {
+        return new Reservation(customer, this, calculateFee(audienceCount), audienceCount);
+    }
+
+    private Money calculateFee(int audienceCount) {
+        //Screening -> Movie : calculateMovieFee 메시지 전송
+        return movie.calculateMovieFee(this).times(audienceCount);
+    }
+
+}
