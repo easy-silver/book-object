2ef80519627341c77845e1611ec4bbe97f83dae4
Timo <developer.timo@gmail.com>
2021-12-13 02:16:45 +0900

    [CHAPTER 05] 책임 할당하기 * 구현을 통한 검증 * '금액을 계산하라'라는 메시지의 책임을 수행하는 영화(Screening) 클래스 구현 * p.148 ~ p.149

---
+++ src/main/java/chap05/DiscoutCondition.java
@@ -0,0 +1,8 @@
+package chap05;
+
+public class DiscoutCondition {
+    public boolean isSatisfiedBy(Screening screening) {
+        return false;
+    }
+
+}
--- src/main/java/chap05/Movie.java
+++ src/main/java/chap05/Movie.java
@@ -1,8 +1,68 @@
 package chap05;
 
+import chap04.MovieType;
+
+import java.time.Duration;
+import java.util.List;
+
+/**
+ * 영화
+ * 책임: '금액을 계산하라' 메시지에 응답할 수 있어야 한다.
+ *
+ */
 public class Movie {
+
+    private String title;
+    private Duration runningTime;
+    private Money fee;
+    private List<DiscoutCondition> discountConditions;
+
+    private MovieType movieType;
+    private Money discountAmount;
+    private double discountPercent;
+
+    //영화 예매 금액을 계산하는 메서드
     public Money calculateMovieFee(Screening screening) {
-        return null;
+        if (isDiscountable(screening)) {
+            return fee.minus(calculateDiscountAmount());
+        }
+        return fee;
     }
+
+    //할인 조건을 순회하며 할인 조건에 해당하는지 확인하는 메서드
+    private boolean isDiscountable(Screening screening) {
+        return discountConditions.stream()
+                .anyMatch(condition -> condition.isSatisfiedBy(screening));
+    }
+
+    //할인 정책을 확인하여 할인 금액 반환
+    private Money calculateDiscountAmount() {
+        switch (movieType) {
+            case AMOUNT_DISCOUNT:
+                return calculateAmountDiscountAmount();
+            case PERCENT_DISCOUNT:
+                return calculatePercentDiscountAmount();
+            case NONE_DISCOUNT:
+                return calculateNoneDiscountAmount();
+        }
+
+        throw new IllegalArgumentException();
+    }
+
+    //금액 할인의 할인 금액 계산
+    private Money calculateAmountDiscountAmount() {
+        return discountAmount;
+    }
+
+    //비율 할인의 할인 금액 계산
+    private Money calculatePercentDiscountAmount() {
+        return fee.times(discountPercent);
+    }
+
+    //할인 정책 미적용의 할인 금액 계산
+    private Money calculateNoneDiscountAmount() {
+        return Money.ZERO;
+    }
+
 
 }
