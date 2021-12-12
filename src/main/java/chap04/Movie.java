fe10aa4da33420e55aa98f1f541c57f04f2a0fa7
Timo <developer.timo@gmail.com>
2021-12-12 11:35:49 +0900

    [CHAPTER 04] 설계 품질과 트레이드 오프 * 데이터 중심의 영화 예매 시스템 * 객체 스스로 자신의 데이터를 책임지기 위해 ReservationAgency 로직을 객체별로 캡슐화 처리 * p.119 ~ p.125

--- src/main/java/chap04/Movie.java
+++ src/main/java/chap04/Movie.java
@@ -1,6 +1,7 @@
 package chap04;
 
 import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -18,6 +19,46 @@ public class Movie {
     private Money discountAmount;
     //비율 할인 정책에 사용되는 데이터
     private double discountPercent;
+
+    public boolean isDiscountable(LocalDateTime whenScreened, int sequence) {
+        for (DiscountCondition condition : discountConditions) {
+            if (condition.getType() == DiscountConditionType.PERIOD) {
+                if (condition.isDiscountable(whenScreened.getDayOfWeek(), whenScreened.toLocalTime())) {
+                    return true;
+                }
+
+            } else {
+                if (condition.isDiscountable(sequence)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public Money calculateAmountDiscountedFee() {
+        if (movieType != MovieType.AMOUNT_DISCOUNT) {
+            throw new IllegalArgumentException();
+        }
+
+        return fee.minus(discountAmount);
+    }
+
+    public Money calculatePercentDiscountedFee() {
+        if (movieType != MovieType.PERCENT_DISCOUNT) {
+            throw new IllegalArgumentException();
+        }
+
+        return fee.minus(fee.times(discountPercent));
+    }
+
+    public Money calculateNoneDiscountedFee() {
+        if (movieType != MovieType.NONE_DISCOUNT) {
+            throw new IllegalArgumentException();
+        }
+
+        return fee;
+    }
 
     public String getTitle() {
         return title;
--- src/main/java/chap04/ReservationAgency.java
+++ src/main/java/chap04/ReservationAgency.java
@@ -2,48 +2,16 @@ package chap04;
 
 public class ReservationAgency {
 
+    /**
+     * 영화 예약
+     *
+     * @param screening
+     * @param customer
+     * @param audienceCount
+     * @return
+     */
     public Reservation reserve(Screening screening, Customer customer, int audienceCount) {
-        Movie movie = screening.getMovie();
-
-        //할인 가능 여부 확인
-        boolean discountable = false;
-
-        for (DiscountCondition condition : movie.getDiscountConditions()) {
-            if (condition.getType() == DiscountConditionType.PERIOD) {
-                discountable = screening.getWhenScreened().getDayOfWeek().equals(condition.getDayOfWeek())
-                        && condition.getStartTime().compareTo(screening.getWhenScreened().toLocalTime()) <= 0
-                        && condition.getEndTime().compareTo(screening.getWhenScreened().toLocalTime()) >= 0;
-            } else {
-                discountable = condition.getSequence() == screening.getSequence();
-            }
-
-            if (discountable) {
-                break;
-            }
-        }
-
-        //할인 정책에 따라 예매 요금 계싼
-        Money fee;
-        if (discountable) {
-            Money discountAmount = Money.ZERO;
-
-            switch (movie.getMovieType()) {
-                case AMOUNT_DISCOUNT:
-                    discountAmount = movie.getDiscountAmount();
-                    break;
-                case PERCENT_DISCOUNT:
-                    discountAmount = movie.getFee().times(movie.getDiscountPercent());
-                    break;
-                case NONE_DISCOUNT:
-                    discountAmount = Money.ZERO;
-                    break;
-            }
-
-            fee = movie.getFee().minus(discountAmount);
-        } else {
-            fee = movie.getFee();
-        }
-
+        Money fee = screening.calculateFee(audienceCount);
         return new Reservation(customer, screening, fee, audienceCount);
     }
 
