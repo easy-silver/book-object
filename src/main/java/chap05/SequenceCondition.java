e5c9a854ae06481710ab592e7fb01e95d8138eaa
Timo <developer.timo@gmail.com>
2021-12-14 00:30:25 +0900

    [CHAPTER 05] 책임 할당하기 * 구현을 통한 검증 * 할인 조건(DiscountCondition) 클래스를 '기간 할인 조건', '순번 할인 조건' 클래스로 분리 * p.153 ~ p.155

---
+++ src/main/java/chap05/SequenceCondition.java
@@ -0,0 +1,17 @@
+package chap05;
+
+/**
+ * 순번 할인 조건
+ */
+public class SequenceCondition {
+
+    private int sequence;
+
+    public SequenceCondition(int sequence) {
+        this.sequence = sequence;
+    }
+
+    public boolean isSatisfiedBy(Screening screening) {
+        return sequence == screening.getSequence();
+    }
+}
