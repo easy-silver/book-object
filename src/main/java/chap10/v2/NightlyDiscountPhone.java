c5effda442d3e7659ce272306bef76acc0c34d89
Timo <developer.timo@gmail.com>
2021-12-21 10:48:26 +0900

    [CHAPTER 10] 상속과 코드 재사용 * 통화 요금 계산 애플리케이션 예제 - 상속으로 이용해서 중복 코드 제거하기 * 심야 할인 요금 계산을 위해 Phone을 상속한 NightlyDiscountPhone 하위 클래스 구현 * p.318 ~ p.320

---
+++ src/main/java/chap10/v2/NightlyDiscountPhone.java
@@ -0,0 +1,39 @@
+package chap10.v2;
+
+import chap02.Money;
+import chap10.v1.Call;
+
+import java.time.Duration;
+
+/**
+ * 심야 할인 요금제 계산을 담당하는 클래스
+ * (일반 요금제를 계산하는 Phone 클래스를 상속받아 코드 재사용)
+ */
+public class NightlyDiscountPhone extends Phone {
+    private static final int LATE_NIGHT_HOUR = 22;
+
+    private Money nightlyAmount;
+
+    public NightlyDiscountPhone(Money amount, Duration seconds, Money nightlyAmount) {
+        super(amount, seconds);
+        this.nightlyAmount = nightlyAmount;
+    }
+
+    @Override
+    public Money calculateFee() {
+        //부모 클래스의 calculateFee 호출
+        Money result = super.calculateFee();
+
+        Money nightlyFee = Money.ZERO;
+        for (Call call : getCalls()) {
+            if (call.getFrom().getHour() >= LATE_NIGHT_HOUR) {
+                nightlyFee = nightlyFee.plus(
+                        getAmount().minus(nightlyAmount).times(
+                                call.getDuration().getSeconds() / getSeconds().getSeconds()));
+            }
+        }
+
+        return result.minus(nightlyFee);
+    }
+
+}
---
+++ src/main/java/chap10/v2/Phone.java
@@ -0,0 +1,51 @@
+package chap10.v2;
+
+import chap02.Money;
+import chap10.v1.Call;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 일반 요금 계산을 담당하는 클래스
+ */
+public class Phone {
+    //단위 요금
+    private Money amount;
+    //단위 시간
+    private Duration seconds;
+    //전체 통화 목록
+    private List<Call> calls = new ArrayList<>();
+
+    public Phone(Money amount, Duration seconds) {
+        this.amount = amount;
+        this.seconds = seconds;
+    }
+
+    public void call(Call call) {
+        calls.add(call);
+    }
+
+    public List<Call> getCalls() {
+        return calls;
+    }
+
+    public Money getAmount() {
+        return amount;
+    }
+
+    public Duration getSeconds() {
+        return seconds;
+    }
+
+    public Money calculateFee() {
+        Money result = Money.ZERO;
+
+        for (Call call : calls) {
+            result = result.plus(amount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+        }
+
+        return result;
+    }
+}
