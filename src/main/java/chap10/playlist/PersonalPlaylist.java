b1dfbffec44fe628745a79db4e17d955df025cae
Timo <developer.timo@gmail.com>
2021-12-21 12:07:07 +0900

    [CHAPTER 10] 상속과 코드 재사용 * 플레이리스트 예제 * 가수별 노래 목록 저장 기능 추가 시 하위 클래스의 변경 영향도 확인 * p.330 ~ p.331

--- src/main/java/chap10/playlist/PersonalPlaylist.java
+++ src/main/java/chap10/playlist/PersonalPlaylist.java
@@ -8,5 +8,6 @@ public class PersonalPlaylist extends Playlist {
     //플레이리스트에서 노래 삭제 기능
     public void remove(Song song) {
         getTracks().remove(song);
+        getSigners().remove(song.getSinger());
     }
 }
--- src/main/java/chap10/playlist/Playlist.java
+++ src/main/java/chap10/playlist/Playlist.java
@@ -1,20 +1,27 @@
 package chap10.playlist;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 음악 목록을 저장하는 클래스
  */
 public class Playlist {
     private List<Song> tracks = new ArrayList<>();
+    private Map<String, String> signers = new HashMap<>();
 
     public void append(Song song) {
         getTracks().add(song);
+        signers.put(song.getSinger(), song.getTitle());
     }
 
     public List<Song> getTracks() {
         return tracks;
     }
 
+    public Map<String, String> getSigners() {
+        return signers;
+    }
 }
