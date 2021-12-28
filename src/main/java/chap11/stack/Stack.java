c5510bea77090a7553c9dd22d15db015db7b0739
Timo <developer.timo@gmail.com>
2021-12-28 10:37:18 +0900

    [CHAPTER 11] 합성과 유연한 설계 * 불필요한 인터페이스 상속 문제 - Stack * Vector 클래스를 상속에서 합성 관계로 변경 * p.349

---
+++ src/main/java/chap11/stack/Stack.java
@@ -0,0 +1,23 @@
+package chap11.stack;
+
+import java.util.EmptyStackException;
+import java.util.Vector;
+
+/**
+ * Velctor를 합성 관계로 사용한 Stack 클래스
+ */
+public class Stack<E> {
+    private Vector<E> elements = new Vector<>();
+
+    public E push(E item) {
+        elements.addElement(item);
+        return item;
+    }
+
+    public E pop() {
+        if (elements.isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return elements.remove(elements.size() - 1);
+    }
+}
