5b00f1e46aba1d9009cc8050b36ea05a54b1e3fb
Timo <developer.timo@gmail.com>
2021-12-30 19:55:32 +0900

    [CHAPTER 11] 합성과 유연한 설계 * 상속으로 인한 조합의 폭발적인 증가 * 통화 요금 계산 에제 - 상속을 이용해서 기본 정책 구현하기 * 추상 클래스인 Phone을 상속 받아 기본 요금, 심야 할인 요금 구현 * p.355 ~ p.357

---
+++ src/main/java/chap11/phone/Call.java
@@ -0,0 +1,33 @@
+package chap11.phone;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+/**
+ * 개별 통화 기간을 저장하는 클래스
+ */
+public class Call {
+
+    //통화 시작 시간
+    private final LocalDateTime from;
+    //통화 종료 시간
+    private final LocalDateTime to;
+
+    public Call(LocalDateTime from, LocalDateTime to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    //통화 시간
+    public Duration getDuration() {
+        return Duration.between(from, to);
+    }
+
+    public LocalDateTime getFrom() {
+        return from;
+    }
+
+    public LocalDateTime getTo() {
+        return to;
+    }
+}
---
+++ src/main/java/chap11/phone/Phone.java
@@ -0,0 +1,22 @@
+package chap11.phone;
+
+import chap02.Money;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Phone {
+
+    private List<Call> calls = new ArrayList<>();
+
+    public Money calculateFee() {
+        Money result = Money.ZERO;
+
+        for (Call call : calls) {
+            result = result.plus(calculateCallFee(call));
+        }
+        return result;
+    }
+
+    abstract protected Money calculateCallFee(Call call);
+}
