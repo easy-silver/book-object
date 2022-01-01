ea5fc164bda99a490e401d0054e3694266b747d2
Timo <developer.timo@gmail.com>
2022-01-01 21:18:21 +0900

    [CHAPTER 11] 합성과 유연한 설계 * 상속으로 인한 조합의 폭발적인 증가 * 통화 요금 계산 에제 - 기본 정책에 기본 요금 할인 정책 조합하기 * 심야 할인 요금제에 세금 계산과 기본 요금 할인 정책을 함께 적용한 사례 추가 * p.365 ~ p.366

---
+++ src/main/java/chap11/phone/discount/RateDiscountableAndTaxableNightlyDiscountPhone.java
@@ -0,0 +1,26 @@
+package chap11.phone.discount;
+
+import chap02.Money;
+
+import java.time.Duration;
+
+/**
+ * 심야 할인 요금제의 계산 결과에
+ * 기본 요금 할인 정책을 적용한 후
+ * 세금 정책을 적용
+ */
+public class RateDiscountableAndTaxableNightlyDiscountPhone extends RateDiscountableNightlyDiscountPhone {
+
+    private double taxRate;
+
+    public RateDiscountableAndTaxableNightlyDiscountPhone(Money nightlyAmount, Money regularAmount, Duration seconds,
+                                                          Money discountAmount, double taxRate) {
+        super(nightlyAmount, regularAmount, seconds, discountAmount);
+        this.taxRate = taxRate;
+    }
+
+    @Override
+    protected Money afterCalculated(Money fee) {
+        return super.afterCalculated(fee).plus(fee.times(taxRate));
+    }
+}
---
+++ src/main/java/chap11/phone/tax/TaxableAndRateNightlyDiscountablePhone.java
@@ -0,0 +1,25 @@
+package chap11.phone.tax;
+
+import chap02.Money;
+
+import java.time.Duration;
+
+/**
+ * 심야 할인 요금제의 계산 결과에
+ * 세금 정책을 적용한 후
+ * 기본 요금 할인 정책을 적용
+ */
+public class TaxableAndRateNightlyDiscountablePhone extends TaxableNightlyDiscountPhone {
+    private Money discountAmount;
+
+    public TaxableAndRateNightlyDiscountablePhone(Money nightlyAmount, Money regularAmount, Duration seconds,
+                                                  double taxRate, Money discountAmount) {
+        super(nightlyAmount, regularAmount, seconds, taxRate);
+        this.discountAmount = discountAmount;
+    }
+
+    @Override
+    protected Money afterCalculated(Money fee) {
+        return super.afterCalculated(fee).minus(discountAmount);
+    }
+}
