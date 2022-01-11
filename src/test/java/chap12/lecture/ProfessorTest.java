80c3cb395a661c4f75b63d720488765b234bdcfa
Timo <developer.timo@gmail.com>
2022-01-11 09:09:21 +0900

    [CHAPTER 12] 다형성 * 상속을 사용한 강의 평가 - 업캐스팅과 동적 바인딩 * Lecture 변수에 GradeLecture 인스턴스를 할당하여 업캐스팅 적용 * p.404

--- src/test/java/chap12/lecture/ProfessorTest.java
+++ src/test/java/chap12/lecture/ProfessorTest.java
@@ -10,7 +10,14 @@ class ProfessorTest {
     void compileStatistics() {
         //given
         Professor professor = new Professor("다익스트라",
-                new Lecture("알고리즘", 70, Arrays.asList(81, 95, 75, 50, 45)));
+                new GradeLecture("알고리즘",
+                        70,
+                        Arrays.asList(new Grade("A", 100, 95),
+                                new Grade("B", 94, 80),
+                                new Grade("C", 79, 70),
+                                new Grade("D", 69, 50),
+                                new Grade("F", 49, 0)),
+                        Arrays.asList(81, 95, 75, 50, 45)));
 
         //when
         String statistics = professor.compileStatistics();
